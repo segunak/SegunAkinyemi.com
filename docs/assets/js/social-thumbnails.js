0ef72b4b427cdab67ce508e92ca1a3f276aedd6c
Segun <akintundejr4@gmail.com>
2022-07-30 13:19:39 -0400

    fixing thumbnails

---
+++ docs/assets/js/social-thumbnails.js
@@ -0,0 +1,16 @@
+
+
+console.log("I am here from social thumbnails");
+
+let headTag = document.querySelector("head");
+let metaImageTag = document.querySelector('meta[property="og:image"]');
+
+console.log(metaImageTag);
+
+// const link = document.createElement('link');
+// link.href = 'href';
+// link.rel = 'rel';
+
+
+
+//window.onload = setGitHubCard;
