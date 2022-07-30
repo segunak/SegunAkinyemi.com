6ce4eebc9fa5fd9db266cbeb53afb9b566b15ed8
Segun <akintundejr4@gmail.com>
2022-07-30 14:32:47 -0400

    Making the code simpler

--- docs/assets/js/social-thumbnails.js
+++ docs/assets/js/social-thumbnails.js
@@ -2,13 +2,12 @@ let metaImageTag = document.querySelector('meta[property="og:image"]');
 
 if (metaImageTag) { 
     console.log("Found a meta tag with og:image. Creating equivalent twitter:image meta tag");
-    let headTag = document.querySelector("head");
     let imageURL = metaImageTag.getAttribute("content");
     let twitterImageTag = document.createElement("meta");
 
     twitterImageTag.setAttribute("name", "twitter:image");
     twitterImageTag.setAttribute("content", imageURL);
-    headTag.appendChild(twitterImageTag);
+    document.head.appendChild(twitterImageTag);
 }
 
 //window.onload = setGitHubCard;
