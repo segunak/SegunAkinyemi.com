4b51327d42ec220e612e071f0fe5865ac60410db
Segun <akintundejr4@gmail.com>
2024-06-01 12:22:39 -0400

    even shorter

--- docs/assets/js/sitewide.js
+++ docs/assets/js/sitewide.js
@@ -173,5 +173,5 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
         // Initialize the theme switcher button state
         initializeThemeSwitcherButton();
-    }, 5);
+    }, 2);
 });
